8d3f4f39abb4a9d87d9cff18f35028f51cd6b310
Rassokhin Anton <antonrassohin@gmail.com>
2022-01-23 14:35:02 +0300

    Добавьте файлы проекта.

---
+++ Lab19WpfApp_Homework/Models/Calculate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19WpfApp_Homework.Models
+{
+    static class Calculate
+    {
+        public static double Add(double a) => a*2* Math.PI ;
+    }
+}
---
+++ Lab19WpfApp_Homework/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Lab19WpfApp_Homework.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab19WpfApp_Homework.ViewModels
+{
+    class MainWindowViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Number2 = Calculate.Add(Number1);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+
+            if (Number1 > 0)
+                return true; 
+            else
+                return false;
+        }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+
+
+    }
+}
